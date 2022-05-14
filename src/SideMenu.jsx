9def95b0b922b9b1a404244d1844434c33cbc64e
Sidik Home PC <sidik.riders@gmail.com>
2022-05-14 18:56:24 +0700

    hide tube editor menu when playing

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -18,29 +18,33 @@ const SideMenu = (props) => {
       <aside className="menu">
         <p className="menu-label">Options</p>
         <ul className="menu-list">
-          <li>
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                props.addTube();
-              }}
-            >
-              <i className="fas fa-plus"></i>
-              <span>Add Tube</span>
-            </a>
-          </li>
-          {!props.editing && (
-            <li>
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  props.setEditing();
-                }}
-              >
-                <i className="fas fa-edit"></i>
-                <span>Edit Tubes</span>
-              </a>
-            </li>
+          {!props.isPlaying && (
+            <React.Fragment>
+              <li>
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    props.addTube();
+                  }}
+                >
+                  <i className="fas fa-plus"></i>
+                  <span>Add Tube</span>
+                </a>
+              </li>
+              {!props.editing && (
+                <li>
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      props.setEditing();
+                    }}
+                  >
+                    <i className="fas fa-edit"></i>
+                    <span>Edit Tubes</span>
+                  </a>
+                </li>
+              )}
+            </React.Fragment>
           )}
           <li>
             <a
@@ -51,6 +55,17 @@ const SideMenu = (props) => {
             >
               <i className="fas fa-print"></i>
               <span>Print Tubes</span>
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                props.confirmSolving();
+              }}
+            >
+              <i className="fas fa-dice"></i>
+              <span>Solve Puzzle</span>
             </a>
           </li>
         </ul>

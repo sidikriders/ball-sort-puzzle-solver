7e34a53c3bc43b0147abc2e849bdeaaaf55381b3
Sidik Home PC <sidik.riders@gmail.com>
2022-05-16 21:10:47 +0700

    update color based on PC monitor

--- js/colors.js
+++ js/colors.js
@@ -14,18 +14,36 @@
 //   purple: "#a52182",
 // };
 
+// try as similar as possiblle from laptop
+// const COLORS = {
+//   darkgreen: "#046307",
+//   rosegold: "#FEA3AA",
+//   green: "#07A90F",
+//   pink: "#F778A1",
+//   red: "#FF0000",
+//   brown: "#C04000",
+//   orange: "#E56717",
+//   bluelight: "#40E0D0",
+//   white: "#FFFFFF",
+//   yellow: "#FDD017",
+//   bluegreen: "#045F5F",
+//   blue: "#1569C7",
+//   purple: "#800080",
+// };
+
+// try as similar as possiblle from PC
 const COLORS = {
-  darkgreen: "#046307",
-  rosegold: "#FEA3AA",
-  green: "#07A90F",
-  pink: "#F778A1",
+  darkgreen: "#008000",
+  rosegold: "#FA8072",
+  green: "#20e120",
+  pink: "#FF1493",
   red: "#FF0000",
-  brown: "#C04000",
-  orange: "#E56717",
-  bluelight: "#40E0D0",
+  brown: "#8B4513",
+  orange: "#FF8C00",
+  bluelight: "#00FFFF",
   white: "#FFFFFF",
-  yellow: "#FDD017",
-  bluegreen: "#045F5F",
-  blue: "#1569C7",
-  purple: "#800080",
+  yellow: "#FFF000",
+  bluegreen: "#20B2AA",
+  blue: "#015eb9",
+  purple: "#872d87",
 };
